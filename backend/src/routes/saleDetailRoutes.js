d9f06f29b747aecf396c521fb8c8fa6dc342c09e
GilangAlfarizi <alfarizigilang666@gmail.com>
2025-01-07 13:25:32 +0700

    fix: all saleId and userId param to body

--- backend/src/routes/saleDetailRoutes.js
+++ backend/src/routes/saleDetailRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const saleDetailController = require("../controllers/saleDetailController");
 
-router.get("/:saleId", saleDetailController.getSaleDetail);
+router.get("/", saleDetailController.getSaleDetail);
 router.post("/", saleDetailController.addProduct);
 router.delete("/:id", saleDetailController.deleteSaleDetail);
 
--- backend/src/routes/saleRoutes.js
+++ backend/src/routes/saleRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const saleController = require("../controllers/saleController");
+const { checkRole } = require("../middleware/authMiddleware");
 
-router.get("/:userId", saleController.getUserSale);
-router.post("/:userId", saleController.create);
+router.get("/", saleController.getUserSale);
+router.post("/", saleController.create);
 router.put("/", saleController.submitSale);
 
-router.get("/", saleController.getAll);
+router.get("/admin", checkRole("ADMIN"), saleController.getAll);
 
 module.exports = router;
