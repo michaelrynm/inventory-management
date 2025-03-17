255653c341a1de0eef94a9cc515a665594ee46a3
michaelynm <michaelmarcelino55@gmail.com>
2025-03-17 19:06:45 +0700

    feat: vercel config

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 
 const apiRoutes = require("./routes");
-app.use(cors());
 app.get("/hello", (req, res) => res.send("hello world"));
 app.use("/api", apiRoutes);
 
